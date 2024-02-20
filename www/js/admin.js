9c6433da5813fa47b80c543c9b353aa031addf5a
Scott Means <smeans@gmail.com>
2024-02-20 09:18:32 +0700

    finalized data extraction

--- www/js/admin.js
+++ www/js/admin.js
@@ -47,6 +47,47 @@ document.addEventListener('DOMContentLoaded', () => {
             return tt;
         });
     }
+    function processAttack(afs, tva) {
+        return Object.fromEntries(afs.map((fn, i) => {
+            switch (fn) {
+                case 'bonus': {
+                    if (!tva[i]) {
+                        return [fn, null];
+                    }
+
+                    const bva = tva[i].split(' vs. ');
+                    return [fn, {
+                        damage: +bva[0],
+                        against: bva[1].split(',').map(v => v.trim())
+                    }];
+                } break;
+
+                default: {
+                    return [fn, tva[i]];
+                } break;
+            }
+        }));
+    }
+
+    const INFERNALS_HEALTH = /(?<hitpoints>\d+)\s+\((?<shroudhp>\+?\d+)\)/;
+
+    function fixupHealth(unit) {
+        const ho = {};
+
+        if (INFERNALS_HEALTH.test(unit.hitpoints)) {
+            const groups = unit.hitpoints.match(INFERNALS_HEALTH).groups;
+
+            ho.shroudhp = +groups.shroudhp;
+            ho.hitpoints = +groups.hitpoints;
+        } else {
+            ho.hitpoints = unit.hitpoints;
+        }
+
+        delete unit.hitpoints;
+
+        unit.health = ho;
+    }
+
     function processUnitRows(cfna, tra) {
         const afi = cfna.indexOf('attackname');
         const bfs = cfna.slice(0, afi);
@@ -61,9 +102,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const tva = parseCells(tda);
 
             if (tda.length < cfna.length) {
-                const af = Object.fromEntries(afs.map((fn, i) => {
-                    return [fn, tva[i]];
-                }));
+                const af = processAttack(afs, tva);
 
                 cu.attacks[nameToID(af.attackname)] = af;
 
@@ -76,14 +115,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
             units[nameToID(bf.name)] = bf;
 
-            const af = Object.fromEntries(afs.map((fn, i) => {
-                return [fn, tva[afi + i]];
-            }));
+            const af = processAttack(afs, tva.slice(afi));
+
             if (af.attackname) {
                 bf.attacks = {
                     [nameToID(af.attackname)]: af
                 };
+            } else {
+                bf.attacks = {};
             }
+
+            fixupHealth(bf);
 
             cu = bf;
         });
@@ -92,6 +134,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function extractUnitJSON(ps) {
+        unitDataJSON.innerText = '';
+
         const ife = document.createElement('iframe');
         document.body.appendChild(ife);
         ife.onload = () => {
@@ -103,7 +147,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             const unitData = processUnitRows(cfna, table.querySelectorAll('tr:not(:has(th))'));
 
-            unitDataJSON.innerText = JSON.stringify(unitData);
+            unitDataJSON.innerText = JSON.stringify(unitData, null, 4);
         }
         ife.contentWindow.document.open();
         ife.contentWindow.document.write(ps);
